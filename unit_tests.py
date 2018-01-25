aa105be059ca3e17284cddd8489ed69530b91580
whusym <whusym@gmail.com>
2018-01-25 18:37:07 -0500

    filename with dash cannot be imported; rename pre-processing to pre_processing

--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,9 @@
-# This is the unittest file for all our methods in pre-processing, training, and testing. 
-
+# This is the unittest file for all our methods in pre-processing, training, and testing.
 import pyspark
 import unittest
+from training import *
 from testing import *
-from pre-processing import *
+# from pre-processing import *
 
 class TestStringMethods(unittest.TestCase):
 
@@ -16,9 +16,9 @@ class TestStringMethods(unittest.TestCase):
 						('doc3',('word3'))
 					)),
 					{
-						'word1': (1,0,0,0), 
-						'word2': (0,1,1,0), 
-						'word3': (0,0,1,1), 
+						'word1': (1,0,0,0),
+						'word2': (0,1,1,0),
+						'word3': (0,0,1,1),
 						'word4': (0,0,0,1)
 					}
 				).collect(),
@@ -31,14 +31,14 @@ class TestStringMethods(unittest.TestCase):
 
 
 class PreprocessingMethods(unittest.TestCase):
-    
+
     def test_RemoveEcxeptAlphabets(self):
         self.assertEqual('a1b,c.d!e?g$h'.upper(), 'ABCDEFGH')
-    
+
     def test_MinimumLength(self):
         new_line = MinimumLength('this is a test to remove word with length less than two', 3)
         self.assertEqual(new_line, 'this test remove word with length less than two')
-    
+
     def test_NotStopWords(self):
         stopwords_List = ['group', 'I', 'like']
         result = NotStopWords('I like working in my data science group, it is cool.', stopwords_List)
@@ -57,7 +57,26 @@ class PreprocessingMethods(unittest.TestCase):
         new_rdd = X_Preprocessing(rdd)
         result =  [['CCAT', 'GCAT']]
         self.assertEqual(new_rdd.collect(), result_rdd)
-        
+
+
+class TestTraining(unittest.TestCase):
+
+	def test_words_to_probs(self):
+		rdd_label = sc.parallelize([['CCAT', 'MCAT'], ['ECAT'], ['GCAT']])
+		rdd_text = sc.parallelize([['word1', 'word2'], ['word3'], ['word5']])
+		returned_label = ['CCAT', 'MCAT', 'ECAT', 'GCAT']
+		returned_text = [(['word1', 'word2'], 'CCAT'), (['word1', 'word2'], 'MCAT'), (['word3'], 'ECAT'), (['word5'], 'GCAT')]
+		self.assertEqual(
+				process_label_text(rdd_label, rdd_text)[0].collect(), returned_label
+			)
+		self.assertEqual(process_label_text(rdd_label, rdd_text)[1].collect(),returned_text)
+
+	def test_add_missing(self):
+		cat = sc.parallelize([['word1'], ['word1', 'word2'], ['word3']])
+		all_dict = sc.parallelize([['word1', 3], ['word4', 5], ['word2', 2], ['word3', 1]])
+		total_cat = [['word1', 2], ['word2', 1], ['word3', 1], ['word4', 0]]
+		self.assertEqual(add_missing(cat, all_dict), total_cat.collect())
+
 
 if __name__ == "__main__":
 	sc = pyspark.SparkContext()
